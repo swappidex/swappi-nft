10a93362a35db0fefadfed2c4a393a030189a299
Joel Liu <83172559+csdtowards@users.noreply.github.com>
2022-05-19 20:19:27 +0800

    remove whitelist and receive PPI token while mint (#1)

--- test/swappi-nft-test.js
+++ test/swappi-nft-test.js
@@ -1,14 +1,28 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+let PPIToken = require(`./PPIToken.sol/PPIToken.json`);
+
 describe("Swappi NFT Smart Contract Tests", function () {
   let swappinft;
   let tokenBaseURI = "https://aliyuncs.com/0";
 
+  let PPITokenContract;
+
   this.beforeEach(async function() {
     // This is executed before each test
+
+    [owner, tokenReceiver] = await ethers.getSigners();
+    console.log(`owner address ${owner.address}`);
+
+    const factory  = new ethers.ContractFactory(PPIToken.abi, PPIToken.bytecode, owner);
+    PPITokenContract = await factory.deploy();
+    await PPITokenContract.deployed();
+
+    console.log(`PPI contract address: ${PPITokenContract.address}`);
+
     const swappiNft = await ethers.getContractFactory("SwappiNFT");
-    swappinft = await swappiNft.deploy("Swappi NFT Contract", "SwappiNFT", 1000, tokenBaseURI);
+    swappinft = await swappiNft.deploy("Swappi NFT Contract", "SwappiNFT", 1000, tokenBaseURI, PPITokenContract.address, tokenReceiver.address, 200);
   })
 
   it("Total supply can be set", async function() {
@@ -22,16 +36,10 @@ describe("Swappi NFT Smart Contract Tests", function () {
     [account1, account2] = await ethers.getSigners();
     expect(await swappinft._contractOwner()).to.equal(account1.address);
 
+    await expect(swappinft.connect(account2).setContractOwner(account2.address)).to.be.revertedWith('SwappiNFT: must be contract owner');
+
     await swappinft.setContractOwner(account2.address);
     expect(await swappinft._contractOwner()).to.equal(account2.address);
-  })
-
-  it("Whitelist can be set", async function() {
-    [account1] = await ethers.getSigners();
-    expect(await swappinft.verifyWhiteList(account1.address)).to.equal(0);
-
-    await swappinft.setWhiteList(account1.address, 5);
-    expect(await swappinft.verifyWhiteList(account1.address)).to.equal(5);
   })
 
   it("Token Base URI can be set", async function() {
@@ -57,33 +65,49 @@ describe("Swappi NFT Smart Contract Tests", function () {
 
     expect(await swappinft.balanceOf(account1.address)).to.equal(0);
     
-    await expect(swappinft.connect(account1).mint()).to.be.revertedWith('SwappiNFT: address must be in whitelist');
+    await expect(swappinft.connect(account1).mint()).to.be.revertedWith('SwappiNFT: TRANSFER_FROM_FAILED');
   })
 
   it("NFT is minted successfully", async function() {
-    [account1] = await ethers.getSigners();
+    [owner, receiver, account1] = await ethers.getSigners();
 
     expect(await swappinft.balanceOf(account1.address)).to.equal(0);
-
-    await swappinft.setWhiteList(account1.address, 1);
     await swappinft.enableMint();
-    
+
+    await PPITokenContract.mint(account1.address, 200);
+    await PPITokenContract.connect(account1).approve(swappinft.address, 200);
+    expect(await PPITokenContract.balanceOf(account1.address)).to.equal(200);
+    expect(await PPITokenContract.balanceOf(receiver.address)).to.equal(0);
+
+    console.log(`Mint address ${account1.address}`);
+
     const tx = await swappinft.connect(account1).mint();
 
     expect(await swappinft.balanceOf(account1.address)).to.equal(1);
+    expect(await PPITokenContract.balanceOf(account1.address)).to.equal(0);
+    expect(await PPITokenContract.balanceOf(receiver.address)).to.equal(200);
+
+    await expect(swappinft.connect(account1).mint()).to.be.revertedWith('SwappiNFT: mint more than once');
   })
 
   it("tokenURI is set sucessfully", async function() {
-    [account1, account2] = await ethers.getSigners();
-
-    await swappinft.setWhiteList(account1.address, 1);
-    await swappinft.setWhiteList(account2.address, 1);
+    [owner, receiver, account1, account2] = await ethers.getSigners();
     await swappinft.enableMint();
+
+    await PPITokenContract.mint(account1.address, 200);
+    await PPITokenContract.connect(account1).approve(swappinft.address, 200);
+
+    await PPITokenContract.mint(account2.address, 200);
+    await PPITokenContract.connect(account2).approve(swappinft.address, 200);
+
+    expect(await PPITokenContract.balanceOf(receiver.address)).to.equal(0);
 
     const tx1 = await swappinft.connect(account1).mint();
     const tx2 = await swappinft.connect(account2).mint();
 
     expect(await swappinft.tokenURI(0)).to.equal(tokenBaseURI);
     expect(await swappinft.tokenURI(1)).to.equal(tokenBaseURI);
+
+    expect(await PPITokenContract.balanceOf(receiver.address)).to.equal(400);
   })
 });
